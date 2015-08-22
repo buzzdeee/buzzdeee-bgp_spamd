97221716dfdc2c1e0f67d9e44d173e958d070bfe
Sebastian Reitenbach <sebastia@l00-bugdead-prods.de>
2015-08-22 20:02:07 +0200

    spamdb accepts mail addresses with uppercase, but converts internally everything into lowercase.
    Because of that, convert the trap mail addresses to lowercase
    before checking if they are already in the db

--- manifests/spamtraps.pp
+++ manifests/spamtraps.pp
@@ -2,9 +2,10 @@ class bgp_spamd::spamtraps (
   $spamtraps,
 ){
   each($spamtraps) |$trap| {
+    $lowercase_trap = downcase($trap)
     exec { "spamtrap $trap":
       command => "/usr/sbin/spamdb -T -a '$trap'",
-      onlyif  => "/bin/[ ! `spamdb | grep SPAMTRAP | grep '$trap'` ]",
+      onlyif  => "/bin/[ ! `spamdb | grep SPAMTRAP | grep '$lowercase_trap'` ]",
     }
   }
 }
